0c0532d5ba62ef63e0c8159f709b3de84bdab9d3
divaarfis <divarfis06@gmail.com>
2026-01-06 10:59:19 +0700

    menambahkan form data diri penelitian

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/data-diri', function () {
+    // layouts (folder) . penelitian (folder) . DataDiri (nama file)
+    return view('layouts.penelitian.DataDiri');
+});
